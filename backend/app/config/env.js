3d55bab6279b39ae22c215f18ad972d5584a4416
ArjunXvarma <arj15835@gmail.com>
2025-01-18 12:10:22 +0530

    Initialised sql packages for express

--- backend/app/config/env.js
+++ backend/app/config/env.js
@@ -4,4 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   API_KEY: process.env.API_KEY || "default-api-key",
   NODE_ENV: process.env.NODE_ENV || "development",
+  DB_USER: process.env.DB_USER || "12th-main-stickers",
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DA_NAME || "sticker-server",
 };
---
+++ backend/app/config/sql.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require("sequelize");
+
+require("dotenv").config();
+
+// Initialize Sequelize
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  },
+);
+
+// Test the connection
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.error("Unable to connect to the database:", err));
+
+module.exports = sequelize;
